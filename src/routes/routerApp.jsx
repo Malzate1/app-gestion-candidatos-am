192770aead0d9e3e8e31c6d01a02e52faa23ba95
Malzate1 <nadja.minni@gmail.com>
2026-03-17 10:22:51 -0500

    (chores): Instalacion de dependencias react router dom y sweetalert2 y configuracion de react router dom.

---
+++ src/routes/routerApp.jsx
@@ -0,0 +1,21 @@
+import Login from "../pages/Login"
+import Dashboard from "../pages/Dashboard"
+import Register from "../pages/Register"
+
+
+export let routerApp=[
+    {
+        path:"/",
+        element:<Login/>
+    },
+    {
+        path:"/dashboard",
+        element:<Dashboard/>
+
+    },
+    {
+        path:"/register",
+        element:<Register/>
+    }
+
+]
